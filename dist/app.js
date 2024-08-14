779a6bb3ca25d80f64b4c57d429fa23ae21349b6
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-08-14 12:44:36 +0500

    add-ons

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,6 @@
 "use strict";
+function navigateToPage(event) {
+    event.preventDefault(); // Formaning default harakatini to'xtatish
+    window.location.href = './static/pages/plan.html';
+}
 console.log(1);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,6 @@
+function navigateToPage(event: any): void {
+    event.preventDefault(); // Formaning default harakatini to'xtatish
+    window.location.href = './static/pages/plan.html';
+}
+
 console.log(1);
